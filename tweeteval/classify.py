7bca5efc6896dc6ccc5b27a43e400c15144fb2a4
Thomas Buhrmann <thomas@graphext.com>
2021-06-09 11:39:47 +0200

    Added dumb TfIdf+LogReg baseline as demo.

--- tweeteval/classify.py
+++ tweeteval/classify.py
@@ -1,4 +1,9 @@
+from sklearn.pipeline import make_pipeline
 from sklearn.base import ClassifierMixin
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import FunctionTransformer
 from transformers import pipeline
 from tqdm.auto import tqdm
 
@@ -33,3 +38,17 @@ class PretrainedCardiffClassifier(ClassifierMixin):
         if self.humanize:
             labels = map_labels(labels, task=self.task)
         return labels
+
+
+def TfidfLogreg(tfidf_cfg=None, svd_cfg=None, lr_cfg=None):
+    """TfIdf followed by LogReg, just a template for dumb baselines."""
+    tfidf_cfg = tfidf_cfg or {}
+    svd_cfg = {**{"n_components": 300}, **(svd_cfg or {})}
+    lr_cfg = {**{"Cs": 10, "max_iter": 500}, **(lr_cfg or {})}
+    preproc = FunctionTransformer(lambda X: [preprocess(t) for t in X])
+    return make_pipeline(
+        preproc,
+        TfidfVectorizer(**tfidf_cfg),
+        TruncatedSVD(**svd_cfg),
+        LogisticRegressionCV(**lr_cfg),
+    )
